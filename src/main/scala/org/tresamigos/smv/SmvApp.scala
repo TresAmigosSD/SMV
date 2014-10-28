2f4b7a3bf35f4e8fc5e8e9b4fe631f223f961ea2
Ali Tajeldin <alitajeldin@yahoo.com>
2014-10-27 21:40:22 -0700

    SMV #70: initial implementation of new SMV application framework V3.

--- src/main/scala/org/tresamigos/smv/SmvApp.scala
+++ src/main/scala/org/tresamigos/smv/SmvApp.scala
@@ -1,7 +1,39 @@
+/*
+ * This file is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.tresamigos.smv
 
 import org.apache.spark.sql.{SchemaRDD, SQLContext}
 import org.apache.spark.{SparkContext, SparkConf}
+
+import scala.collection.mutable
+
+/**
+ * Dependency management unit within the SMV application framework.  Execution order within
+ * the SMV application framework is derived from dependency between SmvDataSet instances.
+ * Instances of this class can either be a file or a module. In either case, there would
+ * be a single result SchemaRDD.
+ */
+abstract class SmvDataSet(val name: String, val description: String = "unknown") {
+  private[smv] val dataDir = sys.env.getOrElse("DATA_DIR", "/DATA_DIR_ENV_NOT_SET")
+
+  /** full path of file associated with this module/file */
+  def fullPath(): String
+
+  /** returns the SchemaRDD from this dataset (file/module) */
+  def rdd(app: SmvApp): SchemaRDD
+}
 
 /**
  * Wrapper around a persistence point.  All input/output files in the application
@@ -9,76 +41,80 @@ import org.apache.spark.{SparkContext, SparkConf}
  * This is true even if the intermedidate step does *NOT* need to be persisted.  The
  * nickname will be used to link modules together.
  */
-case class SmvFile(nickName: String, basePath: String, csvAttributes: CsvAttributes)
+case class SmvFile(_name: String, basePath: String, csvAttributes: CsvAttributes) extends
+    SmvDataSet(_name, s"Input file: ${_name}@${basePath}") {
 
-/**
- * defines the module interface.  Each module must declare the file nicknames it will need
- * as input and the file nicknames it will produce.  The module should *not* persist
- * any SRDD.
- * The run method will be called with a sequence of SRDDs that correspond to the
- * list of required nicknames.  It is expected to produce a sequence of SRDDs that
- * correspond to the list of "provides" nicknames.
- */
-trait SmvModule {
-  def requires() : Seq[String]
-  def provides() : Seq[String]
-  def run(inputs: Map[String, SchemaRDD]) : Map[String, SchemaRDD]
+  override def fullPath() = dataDir + "/" + basePath
+
+  override def rdd(app: SmvApp): SchemaRDD = {
+    implicit val ca = csvAttributes
+
+    app.sqlContext.csvFileWithSchema(fullPath())
+  }
 }
 
 /**
- * Driver for SMV applications.  An application needs to override the getFiles and getModules
- * methods that provide the list of ALL known SmvFiles (in/out) and the list of modules to run.
+ * base module class.  All SMV modules need to extend this class and provide their
+ * name, description and dependency requirements (what does it depend on).
+ * The module run method will be provided the result of all dependent inputs and the
+ * result of the run is the result of the module.  All modules that depend on this module
+ * will be provided the SchemaRDD result from the run method of this module.
+ * Note: the module should *not* persist any RDD itself.
+ */
+abstract class SmvModule(_name: String, _description: String) extends
+  SmvDataSet(_name, _description) {
+
+  def requires() : Seq[String]
+  def run(inputs: Map[String, SchemaRDD]) : SchemaRDD
+
+  // TODO: should probably convert "." in name to path separator "/"
+  override def fullPath() = dataDir + "/output/" + name
+
+  override def rdd(app: SmvApp): SchemaRDD = {
+    run(requires().map(r => (r, app.resolveRDD(r))).toMap)
+  }
+}
+
+/**
+ * Driver for SMV applications.  An application needs to override the getDataSets method
+ * that provide the list of ALL known SmvFiles (in/out) and the list of modules to run.
  */
 abstract class SmvApp (val appName: String, _sc: Option[SparkContext] = None) {
-  private val dataDir = sys.env.getOrElse("DATA_DIR", "/DATA_DIR_ENV_NOT_SET")
+
   val conf = new SparkConf().setAppName(appName)
   val sc = _sc.getOrElse(new SparkContext(conf))
   val sqlContext = new SQLContext(sc)
 
-  def getFiles(): Seq[SmvFile]
-  def getModules(): Seq[SmvModule]
+  /** stack of items currently being resolved.  Used for cyclic checks. */
+  val resolveStack: mutable.Stack[String] = mutable.Stack()
 
-  lazy private val allFilesByName = getFiles.map(f => (f.nickName, f)).toMap
+  /** cache of all RDD that have already been resolved (executed) */
+  val rddCache: mutable.Map[String, SchemaRDD] = mutable.Map()
 
-  /**
-   * Transform sequence of required nicknames in given module to a map from nickname
-   * to input SRDD.
-   */
-  private def getRequiredSRDDs(module: SmvModule) : Map[String, SchemaRDD] = {
-    module.requires().map { n =>
-      val smvFile = allFilesByName(n)
-      implicit val ca = smvFile.csvAttributes
-      (n, sqlContext.csvFileWithSchema(dataDir + "/" + smvFile.basePath))
-    }.toMap
+  /** concrete applications need to provide list of datasets in application. */
+  def getDataSets() : Seq[SmvDataSet]
+
+  lazy private val allDataSetsByName = getDataSets().map(ds => (ds.name, ds)).toMap
+
+  /** Get the RDD associated with given name. */
+  def resolveRDD(name: String) = {
+    if (resolveStack.contains(name))
+      throw new IllegalStateException(s"cycle found while resolving ${name}: " +
+        resolveStack.mkString(","))
+
+    resolveStack.push(name)
+
+    val resRdd = rddCache.getOrElseUpdate(name, allDataSetsByName(name).rdd(this))
+
+    val popRdd = resolveStack.pop()
+    if (popRdd != name)
+      throw new IllegalStateException(s"resolveStack corrupted.  Got ${popRdd}, expected ")
+
+    resRdd
   }
 
-  def getRddByName(rddName: String) = {
-    val smvFile = allFilesByName(rddName)
-    implicit val ca = smvFile.csvAttributes
-    sqlContext.csvFileWithSchema(dataDir + "/" + smvFile.basePath)
-  }
-
-  /**
-   * Persist the output SRDDs from the given module.
-   */
-  private def saveProvidedSRDDs(module: SmvModule, outSRDDs: Map[String, SchemaRDD]) = {
-    val outNames = module.provides()
-    require(outNames.size == outSRDDs.size)
-
-    outNames.foreach { outName =>
-      val outFile = allFilesByName(outName)
-      val srdd = outSRDDs(outName)
-      srdd.saveAsCsvWithSchema(dataDir + "/" + outFile.basePath)(outFile.csvAttributes)
-    }
-  }
-
-  /**
-   * Runs all the provided modules in sequence with no regard to dependency (for now).
-   */
-  def runAll() = {
-    getModules.foreach { m =>
-      saveProvidedSRDDs(m, m.run(getRequiredSRDDs(m)))
-    }
+  def run(name: String) = {
+    resolveRDD(name).saveAsCsvWithSchema(allDataSetsByName(name).fullPath)
   }
 }
 
---
+++ src/test/scala/org/tresamigos/smv/SmvAppTest.scala
@@ -0,0 +1,62 @@
+/*
+ * This file is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tresamigos.smv
+
+import org.apache.spark.sql.SchemaRDD
+
+
+class SmvAppTest extends SparkTestUtil {
+
+  object A extends SmvModule("A", "A Module") {
+    var moduleRunCount = 0
+    override def requires() = Seq.empty
+    override def run(inputs: Map[String, SchemaRDD]) = {
+      moduleRunCount = moduleRunCount + 1
+      require(inputs.size === 0)
+      createSchemaRdd("a:Integer", "1;2;3")
+    }
+  }
+
+  object B extends SmvModule("B", "B Module") {
+    override def requires() = Seq("A")
+    override def run(inputs: Map[String, SchemaRDD]) = {
+      val sc = inputs("A").sqlContext; import sc._
+      require(inputs.size === 1)
+      inputs("A").selectPlus('a + 1 as 'b)
+    }
+  }
+
+  object C extends SmvModule("C", "C Module") {
+    override def requires() = Seq("A", "B")
+    override def run(inputs: Map[String, SchemaRDD]) = {
+      val sc = inputs("A").sqlContext; import sc._
+      require(inputs.size === 2)
+      inputs("B").selectPlus('b + 1 as 'c)
+    }
+  }
+
+  sparkTest("Test normal dependency execution") {
+    object app extends SmvApp("test dependency", Option(sc)) {
+      override def getDataSets() = Seq(A,B,C)
+    }
+
+    val res = app.resolveRDD("C")
+    dumpSRDD(res)
+    assertSrddDataEqual(res, "1,2,3;2,3,4;3,4,5")
+
+    // even though both B and C depended on A, A should have only run once!
+    assert(A.moduleRunCount === 1)
+  }
+}
