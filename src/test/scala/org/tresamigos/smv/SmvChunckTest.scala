cd8ebabad33bfc41a377901ff4ac9420983339f9
ninjapapa <bzhangusc@live.com>
2015-07-14 21:21:07 -0700

    Added the SmvChunckTest

---
+++ src/test/scala/org/tresamigos/smv/SmvChunckTest.scala
@@ -0,0 +1,37 @@
+/*
+ * This file is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tresamigos.smv
+
+class SmvChunkTest extends SparkTestUtil {
+  sparkTest("Test chunkBy") {
+    val ssc = sqlContext; import ssc.implicits._
+    val srdd = createSchemaRdd("k:String;v:String", "k1,a;k1,b;k2,d;k2,c")
+    val runCat = (l: List[Seq[Any]]) => l.map{_(0)}.scanLeft(Seq("")){(a,b) => Seq(a(0) + b)}.tail
+    val runCatFunc = SmvChunkUDF(Seq('v), SmvSchema.fromString("vcat:String"), runCat)
+
+/*
+    val res = srdd.orderBy('k.asc, 'v.asc).chunkBy('k)(runCatFunc)
+    assertSrddSchemaEqual(res, "k:String; vcat:String")
+    assertUnorderedSeqEqual(res.collect.map(_.toString), Seq(
+      "[k1,a]", "[k1,ab]", "[k2,c]", "[k2,cd]"))
+      */
+
+    val res2 = srdd.orderBy('k.asc, 'v.asc).chunkByPlus('k)(runCatFunc)
+    assertSrddSchemaEqual(res2, "k:String; v: String; vcat:String")
+    assertUnorderedSeqEqual(res2.collect.map(_.toString), Seq(
+      "[k1,a,a]", "[k1,b,ab]", "[k2,c,c]", "[k2,d,cd]"))
+  }
+
+}
