4e2a2bc6036072bf71567a26b9d099191ab06956
ninjapapa <bzhangusc@live.com>
2015-08-11 13:39:49 -0700

    Modified example project under docs to used SmvConfig

--- docs/examples/01GetStart/src/main/scala/org/tresamigos/getstart/core/ExampleApp.scala
+++ docs/examples/01GetStart/src/main/scala/org/tresamigos/getstart/core/ExampleApp.scala
@@ -1,34 +1,11 @@
 package org.tresamigos.getstart.core
 
-import org.apache.spark.SparkContext
 import org.tresamigos.smv._
 
-/**
- * Driver for all LungCancer applications.
- * The app name is taken as a parameter to allow individual modules to override the app name
- * in their standalone run mode.
- */
-class ExampleApp(
-                  _args: Seq[String],
-                  _sc: Option[SparkContext] = None
-                  )
-      extends SmvApp(_args, _sc) {
-
-  val num_partitions = sys.env.getOrElse("PARTITIONS", "64")
-  sqlContext.setConf("spark.sql.shuffle.partitions", num_partitions)
-  
-  override val rejectLogger = new SCRejectLogger(sc, 3)
-}
-
+/** define all the raw inputs into the App */
 object ExampleApp {
   val caBar = new CsvAttributes(delimiter = '|', hasHeader = true)
-  
-  /********************************************************************
-   * Product
-   ********************************************************************/
+
   val employment = SmvCsvFile("input/employment/CB1200CZ11.csv", caBar)
-  def main(args: Array[String]) {
-    new ExampleApp(args).run()
-  }
 }
 
