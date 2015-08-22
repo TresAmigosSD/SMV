9993799aadb6c4ceb0868242d4809bbb69cf3f73
Ali Tajeldin <alitajeldin@yahoo.com>
2015-08-22 16:52:00 -0700

    #159: added SmvHDFS.dirList() method in prep for implementing "--purge-old-output" commandl ine option.

--- src/main/scala/org/tresamigos/smv/SmvHDFS.scala
+++ src/main/scala/org/tresamigos/smv/SmvHDFS.scala
@@ -2,6 +2,8 @@ package org.tresamigos.smv
 
 import org.apache.hadoop.fs.FileSystem
 
+import scala.util.Try
+
 /**
  * Some helper HDFS functions.
  */
@@ -9,6 +11,12 @@ object SmvHDFS {
 
   /** default hadoop configuration. */
   private val hadoopConf = new org.apache.hadoop.conf.Configuration()
+
+  /** Return the HDFS FileSystem object than manages the given path. */
+  private def getFileSystem(path: String) = {
+    val uri = java.net.URI.create(path)
+    FileSystem.get(uri, hadoopConf)
+  }
 
   /**
    * delete an HDFS file by given path.
@@ -19,9 +27,23 @@ object SmvHDFS {
    */
   def deleteFile(fileName: String) : Boolean = {
     val path = new org.apache.hadoop.fs.Path(fileName)
-    val uri = java.net.URI.create(fileName)
-    val hdfs = FileSystem.get(uri, hadoopConf)
+    val hdfs = getFileSystem(fileName)
 
     hdfs.delete(path, true)
+  }
+
+  /**
+   * Return a list of files in the given directory.
+   * Note that we don't use hdfs.listFiles as it was not available in earlier
+   * version of hadoop and we don't want to force our users to upgrade.
+   * The only exception would be if we needed to implement recursive directory
+   * walk, then we should switch to listFiles method instead.
+   */
+  def dirList(dirName: String) : Seq[String] = {
+    Try {
+      val path = new org.apache.hadoop.fs.Path(dirName)
+      val hdfs = getFileSystem(dirName)
+      hdfs.listStatus(path).map(_.getPath.getName).toSeq
+    }.getOrElse(Seq.empty[String])
   }
 }
--- src/test/scala/org/tresamigos/smv/SmvHDFSTest.scala
+++ src/test/scala/org/tresamigos/smv/SmvHDFSTest.scala
@@ -1,15 +1,30 @@
 package org.tresamigos.smv
 
-import java.io.File
+import java.io.{PrintWriter, File}
 
 
 class SmvHDFSTest extends SparkTestUtil {
-  sparkTest("Test HDFS delete file operation") {
-    val outFilePath = new File(testDataDir + "SmvHDFSTest/out.csv")
 
-    // create a dummy DF and save it to a scratch file.
-    val df = createSchemaRdd("k:String;", "a;b;c")
-    df.rdd.saveAsTextFile(outFilePath.getAbsolutePath)
+  /** root of all test files in this suite. May or may not already exist! */
+  private val hdfsTestDir = new File(s"${testDataDir}SmvHDFSTest")
+
+  /** wiope out the test directory and recreate an empty instance. */
+  private def resetTestDir = {
+    SmvHDFS.deleteFile(hdfsTestDir.getAbsolutePath)
+    hdfsTestDir.mkdir()
+  }
+
+  /** create dummy file in test dir and return File path */
+  private def createDummyFile(baseName: String): File = {
+    val outFile = new File(hdfsTestDir, baseName)
+    val pw = new PrintWriter(outFile)
+    pw.write("Hello, world")
+    pw.close
+    outFile
+  }
+
+  test("Test HDFS delete file operation") {
+    val outFilePath = createDummyFile("out.csv")
 
     // confirm csv file now exists.
     assert( outFilePath.canRead() )
@@ -24,5 +39,22 @@ class SmvHDFSTest extends SparkTestUtil {
     // second delete should just return false silently.
     val res2 = SmvHDFS.deleteFile(outFilePath.getAbsolutePath())
     assert(!res2)
+  }
+
+  test("Test HDFS list directory operation") {
+    resetTestDir
+
+    createDummyFile("F3.csv")
+    createDummyFile("F1.csv")
+    createDummyFile("F2.csv")
+
+    val files = SmvHDFS.dirList(hdfsTestDir.getAbsolutePath)
+    assertUnorderedSeqEqual(files, Seq("F1.csv", "F2.csv", "F3.csv"))
+  }
+
+  test("Test HDFS list non-existant directory") {
+    resetTestDir
+    val files = SmvHDFS.dirList("/Some_Directory_That_Does_Not_Exist")
+    assert(files === Seq.empty[String])
   }
 }
