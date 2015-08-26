74b00847b3c2a946a4c6b32893f0b4605bed3dd2
Ali Tajeldin <alitajeldin@yahoo.com>
2015-08-26 15:48:22 -0700

    #169: extract tools/_env.sh in prep for creating run_shell.sh

---
+++ tools/_env.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# setup common environment used by run_app.sh and run_shell.sh
+#
+# The following vars will be set once this script is sourced:
+# ARGS : command line args (other than the spark args which are extracted separately)
+# USER_CMD : name of the script that was launched (caller of this script)
+# MASTER : the user specified --master spark arg (or a valid default)
+# SMV_APP_CLASS : user specified --class name to use for spark-submit or SmvApp as default
+# APP_JAR : user specified --jar option or the discovered application fat jar.
+#
+
+declare -a ARGS
+ARGS=($@)
+USER_CMD=`basename $0`
+
+# extract an argument of the form "--arg arg_val" from the command line arguments
+# Return the arg_val if found (and remove the flag and value from ARGS), else return
+# the passed in default value.
+# NOTE: assumes there is an array named ARGS that is set to the command line arguments.
+# USAGE: extract_arg dest_var_name argument_flag default_value
+# Example: extract_arg MASTER --master localhost
+# the above will look for "--master value" in args and if found, set MASTER to value,
+# otherwise, set MASTER to default value "localhost"
+function extract_arg()
+{
+    local arg_var="$1"
+    local arg_name="$2"
+    local arg_default="$3"
+
+    local arg_val=""
+    for i in ${!ARGS[@]}; do
+      if [ "${ARGS[i]}" = "$arg_name" ]; then
+        arg_val="${ARGS[i+1]}"
+        unset "ARGS[i]" "ARGS[i+1]"
+        break
+      fi
+    done
+
+    if [ -z "$arg_val" ]; then
+      arg_val="$arg_default"
+    fi
+
+    eval $arg_var="'$arg_val'"
+}
+
+function find_fat_jar()
+{
+  # find latest fat jar in target directory.
+  APP_JAR=`ls -1t target/*jar-with-dependencies.jar 2>/dev/null| head -1`
+  if [ -z "$APP_JAR" ]; then
+    echo "ERROR: could not find an app jar in target directory"
+    exit 1
+  fi
+}
+
+function extract_spark_args()
+{
+    extract_arg MASTER --master 'local[*]'
+    extract_arg SMV_APP_CLASS --class "org.tresamigos.smv.SmvApp"
+    extract_arg APP_JAR --jar ""
+
+    if [ -z "$APP_JAR" ]; then
+        find_fat_jar
+    fi
+}
+
+# --- MAIN ---
+extract_spark_args
--- tools/run_app.sh
+++ tools/run_app.sh
@@ -7,7 +7,7 @@
 # the rest of the arguments are the standard SmvApp arguments.
 
 set -e
-if [ $# -lt 1 -o "$1" = "-h" ]; then
+if [ "$1" = "-h" ]; then
   echo "USAGE: `basename $0` [-h] [spark_args] <smv_app_args>"
   echo "spark_args:"
   echo "    [--master master]"
@@ -17,66 +17,11 @@ if [ $# -lt 1 -o "$1" = "-h" ]; then
   echo "    [--purge-old-output]"
   echo "    [--data-dir dir] ..."
   echo "    <-m mod1 [mod2 ...] | -s stage1 [stage2 ...] | --run-app> ..."
-  exit 1
+  exit 0
 fi
 
-declare -a ARGS
-ARGS=($@)
-
-# extract an argument of the form "--arg arg_val" from the command line arguments
-# Return the arg_val if found (and remove the flag and value from ARGS), else return
-# the passed in default value.
-# NOTE: assumes there is an array named ARGS that is set to the command line arguments.
-# USAGE: extract_arg dest_var_name argument_flag default_value
-# Example: extract_arg MASTER --master localhost
-# the above will look for "--master value" in args and if found, set MASTER to value,
-# otherwise, set MASTER to default value "localhost"
-function extract_arg()
-{
-    local arg_var="$1"
-    local arg_name="$2"
-    local arg_default="$3"
-
-    local arg_val=""
-    for i in ${!ARGS[@]}; do
-      if [ "${ARGS[i]}" = "$arg_name" ]; then
-        arg_val="${ARGS[i+1]}"
-        unset "ARGS[i]" "ARGS[i+1]"
-        break
-      fi
-    done
-
-    if [ -z "$arg_val" ]; then
-      arg_val="$arg_default"
-    fi
-
-    eval $arg_var="'$arg_val'"
-}
-
-function find_fat_jar()
-{
-  # find latest fat jar in target directory.
-  APP_JAR=`ls -1t target/*jar-with-dependencies.jar 2>/dev/null| head -1`
-  if [ -z "$APP_JAR" ]; then
-    echo "ERROR: could not find an app jar in target directory"
-    exit 1
-  fi
-}
-
-function extract_spark_args()
-{
-    extract_arg MASTER --master 'local[*]'
-    extract_arg SMV_APP_CLASS --class "org.tresamigos.smv.SmvApp"
-    extract_arg APP_JAR --jar ""
-
-    if [ -z "$APP_JAR" ]; then
-        find_fat_jar
-    fi
-}
-
-# ---- MAIN ----
-
-extract_spark_args
+SMV_TOOLS="$(cd "`dirname "$0"`"; pwd)"
+source $SMV_TOOLS/_env.sh
 
 echo "START RUN =============================="
 
