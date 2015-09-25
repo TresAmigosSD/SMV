058ad7456c03e98d1dfaea5857198ae94c08b3a0
Ali Tajeldin <alitajeldin@yahoo.com>
2015-09-25 13:00:38 -0700

    #195: run the sample app during test phase.

---
+++ src/test/scripts/run-sample-app.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -d "src/test/scripts" ]; then
+    echo "Must run this script from top level SMV directory"
+    exit 1
+fi
+
+TEST_DIR=target/test-sample
+APP_NAME=MyApp
+APP_DIR=${TEST_DIR}/${APP_NAME}
+MVN=$(type -P mvn || type -P mvn3)
+
+rm -rf ${TEST_DIR}
+mkdir -p ${TEST_DIR}
+cd ${TEST_DIR}
+
+echo "--------- GENERATE SAMPLE APP -------------"
+../../tools/smv_init.sh ${APP_NAME} com.mycompany.${APP_NAME}
+
+echo "--------- BUILD SAMPLE APP -------------"
+cd ${APP_NAME}
+$MVN clean package
+
+echo "--------- RUN SAMPLE APP -------------"
+../../../tools/run_app.sh --run-app
+
+echo "--------- VERIFY SAMPLE APP OUTPUT -------------"
+COUNT=$(cat data/output/com.mycompany.MyApp.stage2.StageEmpCategory_*.csv/part* | wc -l)
+if [ "$COUNT" -ne 52 ]; then
+    echo "Expected 52 lines in output but got $COUNT"
+    exit 1
+fi
+
