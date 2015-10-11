57745844372da2c2d922762f5c97af69a436ff11
bzhangusc <bzhangusc@live.com>
2015-10-10 19:52:12 -0700

    Removed RollupCubeOp.scala. Added sqlContect as an option to SmvApp

--- tools/conf/smv_shell_init.scala
+++ tools/conf/smv_shell_init.scala
@@ -7,7 +7,7 @@ sc.setLogLevel("ERROR")
 // because shell would launch a separate command for each evalutaion which
 // slows down startup considerably.
 // keeping object name short to make the contents easy to access.
-SmvApp.init(Seq("-m", "None").toArray, Option(sc))
+SmvApp.init(Seq("-m", "None").toArray, Option(sc), Option(sqlContext))
 
 object i {
   import org.apache.spark.sql.DataFrame
