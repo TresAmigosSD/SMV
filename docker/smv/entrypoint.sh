6628dbcfc88a1eaa28cd194f810b35c1e0e2c887
lane <lane.barlow@gmail.com>
2016-08-24 02:13:25 +0000

    clean up /docker

--- docker/smv/entrypoint.sh
+++ docker/smv/entrypoint.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
+# !/bin/bash
 
+# ensure that the mvn and ivy caches have been copied to the host mount
 mkdir -p /projects/.ivy2 /projects/.m2
 rsync -r /home/smv/.ivy2/* /projects/.ivy2
 rsync -r /home/smv/.m2/* /projects/.m2
 
+
+# if no params supplied, start bash
 if [[ $# == 0 ]]; then
     bash
+# if params supplied, execute them as a command
 else
     "$@"
 fi
