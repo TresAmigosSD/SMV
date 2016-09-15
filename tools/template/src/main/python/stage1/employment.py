778f6e12e94bb396597b91264fb76be4f4996273
Kai Chen <kai@sorrentocorp.com>
2016-09-15 10:46:37 -0700

    wip(#367: single-stage template example in Python)

--- tools/template/src/main/python/stage1/employment.py
+++ tools/template/src/main/python/stage1/employment.py
@@ -1,5 +1,5 @@
 from smv import *
-from pyspark.sql.functions import col, sum
+from pyspark.sql.functions import col, sum, lit
 
 from _PROJ_CLASS_.stage1 import inputdata
 
@@ -15,3 +15,15 @@ class PythonEmploymentByState(SmvPyModule):
     def run(self, i):
         df = i[inputdata.PythonEmployment]
         return df.groupBy(col("ST")).agg(sum(col("EMP")).alias("EMP"))
+
+
+class PythonEmploymentByStateCategory(SmvPyModule):
+    def description(self):
+        return "Python Employment By State With Category"
+
+    def requiresDS(self):
+        return [PythonEmploymentByState]
+
+    def run(self, i):
+        df = i[PythonEmploymentByState]
+        return df.selectPlus((col("EMP") > lit(1000000)).alias("cat_high_emp"))
