de0f6edde0d71a99311843e77f08793e3b4efb48
Kai Chen <kai@sorrentocorp.com>
2016-10-02 12:46:12 -0700

    test(#383: module comments should not affect hash)

---
+++ src/test/python/testModuleHash.py
@@ -0,0 +1,29 @@
+from smvbasetest import SmvBaseTest
+from smv import SmvPyDataSet
+
+class BaseModule(SmvPyDataSet):
+    def description(self):
+        return """Base class for modules written for testing"""
+    def requiresDS(self):
+        return []
+    def run(self, i):
+        sqlcontext = self.smv.sqlContext
+        from pyspark.sql.types import StructType
+        return sqlContext.createDataFrame(sqlContext._sc.emptyRDD(), StructType([]))
+
+class ModuleHashTest(SmvBaseTest):
+    def test_add_comment_should_not_change_hash(self):
+        a = """
+class A(BaseModule):
+    def add(this, a, b):
+        return a+b
+        """
+        b = """
+class A(BaseModule):
+    # comments should not change hash
+    def add(this, a, b):
+        return a+b
+        """
+        self.assertEquals(BaseModule.hashsource(a), BaseModule.hashsource(b))
+
+    def test_change_dependency_should_change_hash(self):
