0611c4adfd13f4fda25c7d129717caf1ab434982
Kai Chen <kai@sorrentocorp.com>
2016-11-22 10:28:11 -0800

    example(#372: Python can link to Scala, and depend on Scala link)

--- tools/template/src/main/python/stage2/category.py
+++ tools/template/src/main/python/stage2/category.py
@@ -19,11 +19,33 @@ class PythonEmploymentByStateCategory2(SmvPyModule, SmvPyOutput):
     """Python ETL Example: depending on Scala module
     """
 
-    ScalaMod = SmvPyExtDataSet("com.my.stage1.EmploymentByState")
+    ScalaMod = SmvPyExtDataSet("__PROJ_CLASS_.stage1.EmploymentByState")
 
     def requiresDS(self):
         return [self.ScalaMod]
 
     def run(self, i):
         df = i[self.ScalaMod]
+        return df.smvSelectPlus((col("EMP") > lit(1000000)).alias("cat_high_emp"))
+
+class PythonEmploymentByStateCategory3(SmvPyModule, SmvPyOutput):
+    """Python ETL Example: link to a Scala module"""
+
+    def requiresDS(self):
+        return [inputdata.EmploymentByStateLink2]
+
+    def run(self, i):
+        df = i[inputdata.EmploymentByStateLink2]
+        return df.smvSelectPlus((col("EMP") > lit(1000000)).alias("cat_high_emp"))
+
+class PythonEmploymentByStateCategory4(SmvPyModule, SmvPyOutput):
+    """Python ETL Example: depends on a Scala module that's a link itself"""
+
+    ScalaModLink = SmvPyExtDataSet("_PROJ_CLASS_.stage2.input.EmploymentStateLink")
+
+    def requiresDS(self):
+        return [self.ScalaModLink]
+
+    def run(self, i):
+        df = i[self.ScalaModLink]
         return df.smvSelectPlus((col("EMP") > lit(1000000)).alias("cat_high_emp"))
