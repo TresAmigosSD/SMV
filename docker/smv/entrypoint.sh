7c04cd4c677207a7b854ffbd6fd4e568ca509c99
ninjapapa <bzhangusc@live.com>
2016-12-05 09:53:11 +0000

    #442 add a entrypoint in SMV image to start SMV server after the container is created

--- docker/smv/entrypoint.sh
+++ docker/smv/entrypoint.sh
@@ -5,10 +5,17 @@ mkdir -p /projects/.ivy2 /projects/.m2
 rsync -r /home/smv/.ivy2/* /projects/.ivy2
 rsync -r /home/smv/.m2/* /projects/.m2
 
+function start_server() {
+    cd /projects
+    sbt assembly
+    smv-server
+}
 
 # if no params supplied, start bash
 if [[ $# == 0 ]]; then
     bash
+elif [[ $1 == "--server" ]]; then
+    start_server
 # if params supplied, execute them as a command
 else
     "$@"
