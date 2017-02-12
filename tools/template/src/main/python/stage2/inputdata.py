2b0ea0e286fb1fa56c9c345709e325126f8f8f52
laneb <lane.barlow@gmail.com>
2017-02-12 11:53:47 -0800

    SmvPyModuleLink interface fix (#471)

--- tools/template/src/main/python/stage2/inputdata.py
+++ tools/template/src/main/python/stage2/inputdata.py
@@ -1,14 +1,6 @@
 from smv import SmvPyModuleLink, SmvPyExtDataSet
 from _PROJ_CLASS_.stage1 import employment as emp
 
-class EmploymentByStateLink(SmvPyModuleLink):
-    """Example: how to use SmvPyModuleLink"""
-    @classmethod
-    def target(cls):
-        return emp.PythonEmploymentByState
+EmploymentByStateLink = SmvPyModuleLink(emp.PythonEmploymentByState)
 
-class EmploymentByStateLink2(SmvPyModuleLink):
-    """Example: linking to an external dataset"""
-    @classmethod
-    def target(cls):
-        return SmvPyExtDataSet("_PROJ_CLASS_.stage1.EmploymentByState")
+EmploymentByStateLink2 = SmvPyModuleLink(SmvPyExtDataSet("com.projcom.proj.stage1.EmploymentByState"))
