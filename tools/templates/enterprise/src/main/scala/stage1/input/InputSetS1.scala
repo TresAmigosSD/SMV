63b1d5df6ba6b0f97a0e01621035bfc3fa3ba5f8
Georges Hatem <khorkhe@Georgess-MBP.attlocal.net>
2017-02-16 14:34:47 -0800

    delete the scala stages from enterprise smv-init template

--- tools/templates/enterprise/src/main/scala/stage1/input/InputSetS1.scala
+++
@@ -1,7 +0,0 @@
-package _PROJ_CLASS_.stage1.input
-
-import org.tresamigos.smv._
-
-/** define all the raw inputs into the stage */
-
-object employment extends SmvCsvFile("input/employment/CB1200CZ11.csv")
