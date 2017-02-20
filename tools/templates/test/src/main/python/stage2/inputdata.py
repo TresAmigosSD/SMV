1f556c5494ff3273c4355a6c5bf391f5c019ef4c
laneb <lane.barlow@gmail.com>
2017-02-20 14:26:52 -0800

    fix typo

--- tools/templates/test/src/main/python/stage2/inputdata.py
+++ tools/templates/test/src/main/python/stage2/inputdata.py
@@ -3,4 +3,4 @@ from _PROJ_CLASS_.stage1 import employment as emp
 
 EmploymentByStateLink = SmvPyModuleLink(emp.PythonEmploymentByState)
 
-EmploymentByStateLink2 = SmvPyModuleLink(SmvPyExtDataSet("com.projcom.proj.stage1.EmploymentByState"))
+EmploymentByStateLink2 = SmvPyModuleLink(SmvPyExtDataSet("_PROJ_CLASS_.stage1.EmploymentByState"))
