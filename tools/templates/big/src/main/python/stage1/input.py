0a02d45564e4e1590ccf7ea9d9532019e0505f3d
laneb <lane.barlow@gmail.com>
2017-03-08 17:23:15 -0800

    fix typo

--- tools/templates/big/src/main/python/stage1/input.py
+++ tools/templates/big/src/main/python/stage1/input.py
@@ -3,6 +3,6 @@ from pyspark.sql.functions import col, sum, lit
 
 __all__ = ['EmploymentByState']
 
-class Employment(SmvPyCsvFile):
+class input(SmvPyCsvFile):
     def path(self):
         return "input/employment/CB1200CZ11.csv"
