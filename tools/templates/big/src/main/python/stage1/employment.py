a8c35ccfb693dd25458c6352b06d523c0da0e25e
laneb <lane.barlow@gmail.com>
2017-03-08 16:17:46 -0800

    add template for arbitrary size project

---
+++ tools/templates/big/src/main/python/stage1/employment.py
@@ -0,0 +1,16 @@
+from smv import *
+from pyspark.sql.functions import col, sum, lit
+
+__all__ = ['EmploymentByState']
+
+from _PKG_NAME_ import _DEP_NAME_
+
+class _MOD_NAME_(SmvPyModule, SmvPyOutput):
+    """Python ETL Example: employ by state"""
+
+    def requiresDS(self):
+        return [_DEP_NAME_]
+
+    def run(self, i):
+        df = i[_DEP_NAME_]
+        return df.groupBy(col("ST")).agg(sum(col("EMP")).alias("EMP"))
