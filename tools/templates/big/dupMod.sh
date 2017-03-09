aa60b16033dd018eff72dd205c434fb31379e5cb
laneb <lane.barlow@gmail.com>
2017-03-08 17:00:18 -0800

    fix typo

--- tools/templates/big/dupMod.sh
+++ tools/templates/big/dupMod.sh
@@ -4,7 +4,7 @@ DEST=$2
 NUM_MODS=$3
 
 TBNAME=$(basename $TEMPLATE)
-DUPNAME="${TBNAME}0"
+DUPNAME="$(echo $TBNAME | sed "s/\./0./")"
 cp $TEMPLATE "$DEST/$DUPNAME"
 sed -i "" "s/_MOD_NAME_/M0/" "$DEST/$DUPNAME"
 sed -i "" "s/_PKG_NAME_/stage1.input/" "$DEST/$DUPNAME"
@@ -12,7 +12,7 @@ sed -i "" "s/_DEP_NAME_/input/" "$DEST/$DUPNAME"
 
 for((i=1;i<$NUM_MODS;i++)); do
 LASTDUPNAME=$DUPNAME
-DUPNAME="lp$(echo $TBNAME | sed "s/\./$i./")"
+DUPNAME="$(echo $TBNAME | sed "s/\./$i./")"
 cp "$TEMPLATE" "$DEST/$DUPNAME"
 sed -i "" "s/_MOD_NAME_/M$i/" "$DEST/$DUPNAME"
 sed -i "" "s/_PKG_NAME_/stage1.${LASTDUPNAME%.py}/" "$DEST/$DUPNAME"
