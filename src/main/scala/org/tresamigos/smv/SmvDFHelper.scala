37cf90174960a55a13ce9426be773d4fb520b411
Georges Hatem <ghatem@twineanalytics.com>
2017-03-09 19:21:25 -0800

    #515: loosen the constraint of smvUnion. no longer requires the union'ed DFs to have cols with same nullable value

--- src/main/scala/org/tresamigos/smv/SmvDFHelper.scala
+++ src/main/scala/org/tresamigos/smv/SmvDFHelper.scala
@@ -540,7 +540,11 @@ class SmvDFHelper(df: DataFrame) {
 
     df.sqlContext.createDataFrame(res, newSchema)
   }
-   /** Union two data frames that have shared columns, kept columns on both side*/
+   /**
+    * Union two data frames that have shared columns, kept columns on both side
+    * the spark function 'unionAll' does not care whether two df columns have the same `nullable` param,
+    * so neither should smvUnion
+    */
   private[smv] def RowBind(
     dfother: DataFrame): DataFrame = {
     import df.sqlContext.implicits._
@@ -556,12 +560,12 @@ class SmvDFHelper(df: DataFrame) {
     val overlap = df.columns intersect dfother.columns
     val overlapLeftStruct = overlap map {colName =>
       val struct = df.schema.filter(_.name == colName)(0)
-      val structStripMeta = StructField(struct.name, struct.dataType, struct.nullable)
+      val structStripMeta = StructField(struct.name, struct.dataType, false)
       structStripMeta
     }
     val overlapRightStruct = overlap map {colName =>
       val struct = dfother.schema.filter(_.name == colName)(0)
-      val structStripMeta = StructField(struct.name, struct.dataType, struct.nullable)
+      val structStripMeta = StructField(struct.name, struct.dataType, false)
       structStripMeta
     }
     val diffColumns = overlapLeftStruct diff overlapRightStruct
