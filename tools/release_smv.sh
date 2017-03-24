a916c3db622c00736d332cda4cd731ebb0e52252
Ali Tajeldin <alitajeldin@gmail.com>
2017-03-24 15:09:41 -0700

    #538: added automatic tagging and user doc version update to release script

--- tools/release_smv.sh
+++ tools/release_smv.sh
@@ -2,13 +2,11 @@
 # Release the current version of SMV.  This must be run from within the tresamigos:smv
 # docker container to maintain release consistency
 
-# TODO: make sure current repo doesn't have any checkout.
-# TODO: add automatic taging of source
-# TODO: add a -remove flag to remove an old version (tags and github release)
-
+# TODO: create github release automatically.
 
 set -e
 PROG_NAME=$(basename "$0")
+ORIG_DIR=$(pwd)
 SMV_TOOLS="$(cd "`dirname "$0"`"; pwd)"
 SMV_DIR="$(dirname "$SMV_TOOLS")"
 SMV_DIR_BASE="$(basename $SMV_DIR)"
@@ -17,25 +15,28 @@ PROJ_DIR="$(dirname "$SMV_DIR")" # assume parent of SMV directory is the project
 
 function usage()
 {
-  echo "USAGE: ${PROG_NAME} smv_version_to_release(n.n.n.n)"
+  echo "USAGE: ${PROG_NAME} prev_smv_version new_smv_version_to_release(n.n.n.n)"
   exit $1
 }
 
 function parse_args()
 {
   [ "$1" = "-h" ] && usage 0
-  [ $# -ne 1 ] && echo "ERROR: Missing arguments" && usage 1
+  [ $# -ne 2 ] && echo "ERROR: invalid number of arguments" && usage 1
 
-  SMV_VERSION="$1"
-  validate_version
+  PREV_SMV_VERSION="$1"
+  SMV_VERSION="$2"
+  validate_version "$PREV_SMV_VERSION"
+  validate_version "$SMV_VERSION"
 }
 
-# make sure version of the format a.b.c.d where a,b,c,d are all numbers.
+# make sure version is of the format a.b.c.d where a,b,c,d are all numbers.
 function validate_version()
 {
-  local res=$(echo "$SMV_VERSION" | sed -E -e 's/^([0-9]+\.){3}[0-9]+$//')
+  local ver="$1"
+  local res=$(echo "$ver" | sed -E -e 's/^([0-9]+\.){3}[0-9]+$//')
   if [ -n "$res" ]; then
-    echo "ERROR: invalid version format"
+    echo "ERROR: invalid version format: $ver"
     usage 1
   fi
 }
@@ -68,10 +69,38 @@ function find_gnu_tar()
   fi
 }
 
+function check_git_repo()
+{
+  echo "--- checking repo for modified files"
+  cd "${SMV_DIR}"
+  if ! git diff-index --quiet HEAD --; then
+    echo "ERROR: SMV git repo has locally modified files"
+    exit 1
+  fi
+}
+
+function update_docs_version()
+{
+  echo "---- updating docs to version $SMV_VERSION"
+  cd "${SMV_DIR}"
+  find docs/user -name '*.md' \
+    -exec perl -pi -e "s/${PREV_SMV_VERSION}/${SMV_VERSION}/g" \{\} +
+  git commit -a -m "updated user docs to version $SMV_VERSION"
+}
+
+function tag_release()
+{
+  local tag=v"$SMV_VERSION"
+  echo "---- tagging release as $tag"
+  cd "${SMV_DIR}"
+  git tag -a $tag -m "SMV Release $SMV_VERSION on `date +%m/%d/%Y`"
+  git push origin $tag
+}
+
 function create_tar()
 {
   echo "--- create tar image: "
-  echo "DOCKER_SMV_DIR=$DOCKER_SMV_DIR"
+  cd "$ORIG_DIR"
 
   # cleanup some unneeded binary files.
   rm -rf "${SMV_DIR}/project/target" "${SMV_DIR}/project/project"
@@ -88,5 +117,8 @@ function create_tar()
 # ---- MAIN ----
 parse_args "$@"
 find_gnu_tar
+check_git_repo
+update_docs_version
+tag_release
 build_smv
 create_tar
