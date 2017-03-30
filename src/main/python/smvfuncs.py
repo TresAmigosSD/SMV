68afdaf4cc3e41e66a7a7a56bd81e2241bf9c2bc
laneb <lane.barlow@gmail.com>
2017-03-30 14:09:50 -0700

    add docstrings for smvfuncs (#576)

--- src/main/python/smvfuncs.py
+++ src/main/python/smvfuncs.py
@@ -15,19 +15,79 @@ from smv import smvPy
 from pyspark.sql.column import Column
 
 def nGram2(c1, c2):
+    """2-gram UDF with formula (number of overlaped gramCnt)/max(c1.gramCnt, c2.gramCnt)
+
+        Args:
+            c1 (Column): first column
+            c2 (Column): second column
+
+        Returns:
+            (Column): 2-gram
+    """
     return Column(smvPy._jvm.org.tresamigos.smv.smvfuncs.nGram2(c1._jc, c2._jc))
 
 def nGram3(c1, c2):
+    """3-gram UDF with formula (number of overlaped gramCnt)/max(s1.gramCnt, s2.gramCnt)
+
+        Args:
+            c1 (Column): first column
+            c2 (Column): second column
+
+        Returns:
+            (Column): 3-gram
+    """
     return Column(smvPy._jvm.org.tresamigos.smv.smvfuncs.nGram3(c1._jc, c2._jc))
 
 def diceSorensen(c1, c2):
+    """2-gram UDF with formula (2 * number of overlaped gramCnt)/(s1.gramCnt + s2.gramCnt)
+
+        Args:
+            c1 (Column): first column
+            c2 (Column): second column
+
+        Returns:
+            (Column): 2-gram
+    """
     return Column(smvPy._jvm.org.tresamigos.smv.smvfuncs.diceSorensen(c1._jc, c2._jc))
 
 def normlevenshtein(c1, c2):
+    """Levenshtein edit distance metric UDF
+
+        Args:
+            c1 (Column): first column
+            c2 (Column): second column
+
+        Returns:
+            (Column): distances
+    """
     return Column(smvPy._jvm.org.tresamigos.smv.smvfuncs.normlevenshtein(c1._jc, c2._jc))
 
 def jaroWinkler(c1, c2):
+    """Jaro-Winkler edit distance metric UDF
+
+        Args:
+            c1 (Column): first column
+            c2 (Column): second column
+
+        Returns:
+            (Column): distances
+    """
     return Column(smvPy._jvm.org.tresamigos.smv.smvfuncs.jaroWinkler(c1._jc, c2._jc))
 
 def smvFirst(c, nonNull = False):
+    """Variation of Spark "first" which also returns null values
+
+        Since Spark "first" will return the first non-null value, we have to
+        create our version smvFirst which to retune the real first value, even
+        if it's null. Alternatively can return the first non-null value.
+
+        Args:
+            c (Column: column to extract first value from
+            nonNull (bool): If false, return first value even if null.
+                            If true, return first non-null value.
+                            Defaults to false.
+
+        Returns:
+            (object): first value
+    """
     return Column(smvPy._jvm.org.tresamigos.smv.smvfuncs.smvFirst(c._jc, nonNull))
