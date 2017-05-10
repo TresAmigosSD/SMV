1583a7983d98b8f8ed0ef1990cafbc3ce0273092
laneb <lane.barlow@gmail.com>
2017-05-10 14:10:22 -0700

    add module-level docstring for smvshell (#676)

--- src/main/python/smv/smvshell.py
+++ src/main/python/smv/smvshell.py
@@ -10,7 +10,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+"""Helper functions available in SMV's Python shell
+"""
 from smv import SmvApp
 from pyspark.sql import DataFrame
 
