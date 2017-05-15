ce8d496d59cf3ff89259938a2513f94ae3cfff30
ninjapapa <bzhangusc@live.com>
2017-05-14 21:45:27 -0700

    Added smvGetColName helper and added "keys" attr to SmvGroupedData

--- src/test/python/testColumnHelper.py
+++ src/test/python/testColumnHelper.py
@@ -22,6 +22,11 @@ from pyspark.sql import SQLContext, HiveContext
 from pyspark.sql.functions import array, col
 
 class ColumnHelperTest(SmvBaseTest):
+    def test_smvGetColName(self):
+        df = self.createDF("k:String; v:String;", "a,b;c,d;,")
+        self.assertEqual(df.k.smvGetColName(), 'k')
+        self.assertEqual(array(df.k, df.v).smvGetColName(), 'array(k,v)')
+
     def test_smvIsAllIn(self):
         df = self.createDF("k:String; v:String;", "a,b;c,d;,").select(array(col("k"), col("v")).alias("arr"))
         res = df.select(col("arr").smvIsAllIn("a", "b", "c").alias("isFound"))
