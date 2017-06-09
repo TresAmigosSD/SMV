cc35b0078673692b348b4aa5ea4fa77ff7363434
laneb <lane.barlow@gmail.com>
2017-06-09 14:54:05 -0700

    use env variable to suppress Python bytecode instead of sys attribute

--- tools/_pyenv.sh
+++ tools/_pyenv.sh
@@ -8,7 +8,9 @@
 # Not sure yet which way is best practice.
 SMV_HOME="$(cd "`dirname "$0"`/.."; pwd)"
 export PYTHONPATH="$SMV_HOME/src/main/python:$PYTHONPATH"
-
+# Suppress creation of .pyc files. These cause complications with
+# reloading code and have led to discovering deleted modules (#612)
+export PYTHONDONTWRITEBYTECODE=1
 export SPARK_PRINT_LAUNCH_COMMAND=1
 
 function run_pyspark_with () {
