16203c4444290da8d9664d1ef56900424aba6082
laneb <lane.barlow@gmail.com>
2017-09-13 20:09:19 -0700

    make SmvGroupedData importable (#970)

--- src/main/python/smv/__init__.py
+++ src/main/python/smv/__init__.py
@@ -5,6 +5,7 @@ from smv.smvapp import SmvApp
 
 from smv.runconfig import SmvRunConfig
 from smv.csv_attributes import CsvAttributes
+from smv.helpers import SmvGroupedData
 
 # keep old py names for backwards compatibility
 SmvPyCsvFile = SmvCsvFile
