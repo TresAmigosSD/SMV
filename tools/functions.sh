fe6dbf56fc75cc33aaf7ba55ddf5064fc1c740a9
Kai Chen <kai@sorrentocorp.com>
2017-10-30 10:24:58 -0700

    fix: ver_cmp ignores trailing non-digit chars in each part

--- tools/functions.sh
+++ tools/functions.sh
@@ -1,7 +1,15 @@
 # Bash functions for use in smv
 
-# compares version-wise the two parameters (a, b)
-# where a and b are version text that may be dot-separated
+# remove trailing alphanum characters in dot-separated version text
+function sanitize_version () {
+  # match a digit, followed by a letter, "+" or "_," and anything up to a "."
+  # keep just the digit -- essentially removing any trailing alphanum between dots
+  echo $(sed -E 's/([0-9])[_+a-zA-Z][^.]*/\1/g' <<< "$1")
+}
+
+# compares version-wise the two parameters (a, b) after sanitizing
+# using the function above, where a and b are version text that may be
+# dot-separated
 #
 # echoes -1 if a < b,
 #         0 if a == b,
@@ -11,9 +19,10 @@ function ver_cmp () {
     echo 0
     return
   fi
+  local a=$(sanitize_version $1) b=$(sanitize_version $2)
   # split $1 by '.' into ver1 and $2 by '.' into ver2
   local IFS=.
-  local i ver1=($1) ver2=($2) # same as read -r -a ver1 <<<"$1", etc
+  local i ver1=($a) ver2=($b) # same as read -r -a ver1 <<<"$1", etc
   # fill empty fields in ver1 with zeros
   for ((i=${#ver1[@]}; i<${#ver2[@]}; i++)); do
     ver1[i]=0
@@ -35,49 +44,15 @@ function ver_cmp () {
 
 # compares version-wise two the parameters (a,b)
 # where a and b are single version fields, i.e. without dots
-# but may contain alphanumeric letters
+# and contain only digits, which may be 0-padded
 #
 # echoes -1 if a < b, 0 if a == b, and 1 if a > b
 function ver_cmp0() {
-  local a=$1 b=$2 a2=0 b2=0
-  local r1="([0-9]+)[-+a-zA-Z]+$"    # of the form 2arb_text+stuff
-  local r2="([0-9]+)[-+a-zA-Z]+([0-9]+)$" # of the form 2rc1
-
-  if [[ $a =~ $r1 ]]; then
-    a=${BASH_REMATCH[1]}
-  fi
-  if [[ $a =~ $r2 ]]; then
-    a=${BASH_REMATCH[1]}
-    a2=${BASH_REMATCH[2]}
-  fi
-
-  if [[ $b =~ $r1 ]]; then
-    b=${BASH_REMATCH[1]}
-  fi
-  if [[ $b =~ $r2 ]]; then
-    b=${BASH_REMATCH[1]}
-    b2=${BASH_REMATCH[2]}
-  fi
-
-  if (( 10#$a < 10#$b )); then
+  if (( 10#$1 < 10#$2 )); then
     echo -1
-  elif (( 10#$a > 10#$b )); then
+  elif (( 10#$1 > 10#$2 )); then
     echo 1
   else
-    if (( a2 == 0 && b2 == 0 )); then
-      echo 0
-    elif (( a2 == 0 && b2 != 0)); then
-      echo 1  # e.g. 1.2 and 1.2+hot should come after 1.2rc1
-    elif (( a2 != 0 && b2 == 0 )); then
-      echo -1
-    else
-      if (( 10#$a2 < 10#$b2 )); then
-        echo -1
-      elif (( 10#$a2 > 10#$b2 )); then
-        echo 1
-      else
-        echo 0
-      fi
-    fi
+    echo 0
   fi
 }
