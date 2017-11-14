b36c4a8084e1140cd675af58eba0fe9769f90fb6
Kai Chen <kai@sorrentocorp.com>
2017-11-14 10:07:12 -0800

    refact: newly-registered repo factory should be searched first

--- src/main/scala/org/tresamigos/smv/DataSetMgr.scala
+++ src/main/scala/org/tresamigos/smv/DataSetMgr.scala
@@ -29,7 +29,8 @@ class DataSetMgr(smvConfig: SmvConfig) {
   private var allStageNames                            = smvConfig.stageNames
 
   def register(newRepoFactory: DataSetRepoFactory): Unit = {
-    dsRepoFactories = dsRepoFactories :+ newRepoFactory
+    // more recently registered repo factories are searched first
+    dsRepoFactories = newRepoFactory +: dsRepoFactories
   }
 
   /**
