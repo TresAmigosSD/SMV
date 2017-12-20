73d00484d143b849a896b0f3f79fa4ded41611dd
Guangning Yu <hi@guangningyu.com>
2017-12-20 23:37:05 +0800

    add method getMetadataJson in smvserver

--- src/main/scala/org/tresamigos/smv/python/SmvPythonProxy.scala
+++ src/main/scala/org/tresamigos/smv/python/SmvPythonProxy.scala
@@ -255,6 +255,10 @@ class SmvPyClient(val j_smvApp: SmvApp) {
   def runModule(urn: String, forceRun: Boolean, version: Option[String]): DataFrame =
     j_smvApp.runModule(URN(urn), forceRun, version)
 
+  /** Returns metadata for a given urn*/
+  def getMetadataJson(urn: String): String =
+    j_smvApp.getMetadataJson(URN(urn))
+
   // TODO: The following method should be removed when Scala side can
   // handle publish-hive SmvOutput tables
   def moduleNames: java.util.List[String] = {
