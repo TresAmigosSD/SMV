8706900cc5586073b8bdefa46e243876f3acfd20
laneb <lane.barlow@gmail.com>
2018-06-05 14:18:02 -0700

    add comment

--- src/main/scala/org/tresamigos/smv/dqm/DQMTask.scala
+++ src/main/scala/org/tresamigos/smv/dqm/DQMTask.scala
@@ -75,6 +75,20 @@ case class DQMRule(
   val name = if (ruleName == null) rule.toString else ruleName
 
   private[smv] def createCheckCol(dqmState: DQMState): (Column, Column, Column) = {
+    // We go through the Spark internals in order to access the names of the columns referenced
+    // in the rule. The API of `AttributeSet` (which is really not intended for external use like 
+    // this) changed between Spark 2.2.1 and 2.3.0, requiring us to reach a bit deeper for the name. 
+    // This approach is unstable, so we cannot rely on it in the long term. However, the plan is to
+    // remove this framework for DQM altogether, so I've patched it in the short term.
+    //
+    // references is an AttributeSet(basically a set of nodes in the catalyst expression), and we 
+    // need to access the Attributes themselves in order to get their names. We used to be able to 
+    // call references.toSeq to get the list of attributes, but this broke in 2.3.0 - toSeq now tries
+    // to sort the attributes by a property which canoot be accessed if the references are unresolved,
+    // which they will be in the vast majority of DQM uses. In order to work around this, we access 
+    // the AttributeSet's internal representation of the Attributes, baseSet, which is a 
+    // Seq[AttributeEquals]. Each AttributeEquals contains one Attribute, and we map to get their 
+    // names.
     val refCols = rule.toExpr.references.baseSet.toSeq.map { _.a.name }
     
     val catCols = refCols
