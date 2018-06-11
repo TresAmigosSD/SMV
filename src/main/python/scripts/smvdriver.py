8144bf98a712bdf301980877c87853bef7b758bc
laneb <lane.barlow@gmail.com>
2018-06-11 00:34:23 -0700

    add comments

--- src/main/python/scripts/smvdriver.py
+++ src/main/python/scripts/smvdriver.py
@@ -3,6 +3,12 @@ import sys
 from smv import SmvApp
 
 class SmvDriver(object):
+    """Driver for an SMV application
+
+        SmvDriver handles the boiler plate around parsing driver args, constructing an SmvApp, and running an application.
+        To use SmvDriver, override `main` and in the main block of your driver script call construct your driver and
+        call `run`.
+    """
     def create_smv_app(self, smv_args, driver_args):
         return SmvApp.createInstance(smv_args)
 
@@ -18,6 +24,9 @@ class SmvDriver(object):
             smv_args_end = len(args)
 
         smv_args = args[:smv_args_end]
+        # First arg after smv_args_end is --script
+        # Second is the script name
+        # Then the driver args start
         driver_args = args[smv_args_end+2:]
 
         app = self.create_smv_app(smv_args, driver_args)
