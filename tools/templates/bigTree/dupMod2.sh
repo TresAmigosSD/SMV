1bb2f90f1d4899675d17d448f8e0320a3ad1d1a6
qing.li <liqing55liqing@126.com>
2018-06-20 17:38:33 +0800

    #1297: update generate balanced binary tree

--- tools/templates/bigTree/dupMod2.sh
+++ tools/templates/bigTree/dupMod2.sh
@@ -17,10 +17,10 @@ cp "$TEMPLATE" "$DEST/$DUPNAME"
 sed -i "" "s/_MOD_NAME_/M$i/" "$DEST/$DUPNAME"
 VAR=i%2
 if [ $VAR = 1 ]; then
+  sed -i "" "s/_PKG_NAME_/stage1.${TBNAME%.py}$(i/2)/" "$DEST/$DUPNAME"
+  sed -i "" "s/_DEP_NAME_/M$(i/2)/" "$DEST/$DUPNAME"
+else
   sed -i "" "s/_PKG_NAME_/stage1.${TBNAME%.py}$(((i-1)/2))/" "$DEST/$DUPNAME"
   sed -i "" "s/_DEP_NAME_/M$(((i-1)/2))/" "$DEST/$DUPNAME"
-else
-  sed -i "" "s/_PKG_NAME_/stage1.${TBNAME%.py}$(((i-2)/2))/" "$DEST/$DUPNAME"
-  sed -i "" "s/_DEP_NAME_/M$(((i-2)/2))/" "$DEST/$DUPNAME"
 fi
 done
