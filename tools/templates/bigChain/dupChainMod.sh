68707150080eea3b04dc692b52bb4ffee37debbe
qing.li <liqing55liqing@126.com>
2018-06-25 10:03:25 +0800

    #1297: revert to sed -i

--- tools/templates/bigChain/dupChainMod.sh
+++ tools/templates/bigChain/dupChainMod.sh
@@ -7,15 +7,15 @@ NUM_MODS=$3
 TBNAME=$(basename $TEMPLATE)
 DUPNAME="$(echo $TBNAME | sed "s/\./0./")"
 cp $TEMPLATE "$DEST/$DUPNAME"
-perl -pi "" "s/_MOD_NAME_/M0/" "$DEST/$DUPNAME"
-perl -pi "" "s/_PKG_NAME_/stage1.input/" "$DEST/$DUPNAME"
-perl -pi "" "s/_DEP_NAME_/input/" "$DEST/$DUPNAME"
+sed -i "" "s/_MOD_NAME_/M0/" "$DEST/$DUPNAME"
+sed -i "" "s/_PKG_NAME_/stage1.input/" "$DEST/$DUPNAME"
+sed -i "" "s/_DEP_NAME_/input/" "$DEST/$DUPNAME"
 
 for((i=1;i<$NUM_MODS;i++)); do
 LASTDUPNAME=$DUPNAME
 DUPNAME="$(echo $TBNAME | sed "s/\./$i./")"
 cp "$TEMPLATE" "$DEST/$DUPNAME"
-perl -pi "" "s/_MOD_NAME_/M$i/" "$DEST/$DUPNAME"
-perl -pi "" "s/_PKG_NAME_/stage1.${LASTDUPNAME%.py}/" "$DEST/$DUPNAME"
-perl -pi "" "s/_DEP_NAME_/M$((i-1))/" "$DEST/$DUPNAME"
+sed -i "" "s/_MOD_NAME_/M$i/" "$DEST/$DUPNAME"
+sed -i "" "s/_PKG_NAME_/stage1.${LASTDUPNAME%.py}/" "$DEST/$DUPNAME"
+sed -i "" "s/_DEP_NAME_/M$((i-1))/" "$DEST/$DUPNAME"
 done
