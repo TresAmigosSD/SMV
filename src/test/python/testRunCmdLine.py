c0511cdc85ec75c31839b0416d4a1e5ca27b5506
Bo Zhang <bzhangusc@live.com>
2018-09-27 10:40:15 -0700

    #1351 all SmvAppTest moved to python side

--- src/test/python/testRunCmdLine.py
+++ src/test/python/testRunCmdLine.py
@@ -12,34 +12,92 @@
 # limitations under the License.
 
 from test_support.smvbasetest import SmvBaseTest
+from py4j.protocol import Py4JJavaError
 
-
-class RunModuleFromCmdLineTest(SmvBaseTest):
-    modUrn = 'runmod.stage1.modules.A'
+class RunCmtLineBaseTest(SmvBaseTest):
+    @classmethod
+    def whatToRun(cls):
+        ["-m", "None"]
 
     @classmethod
     def smvAppInitArgs(cls):
-        return ['--smv-props', 'smv.stages=runmod.stage1',
-                '-m', "modules.A"]
+        return ['--smv-props', 'smv.stages=runstage.stage1'] + cls.whatToRun()
+
+    
+
+class RunModuleFromCmdLineTest(RunCmtLineBaseTest):
+    @classmethod
+    def whatToRun(cls):
+        return ['-m', "modules.A"]
 
     def test_can_run_module_from_cmdline(self):
         self.smvApp.j_smvApp.run()
-        a = self.df(self.modUrn)
+        a = self.df("runstage.stage1.modules.A")
         expected = self.createDF("k:String;v:Integer", "a,;b,2")
         self.should_be_same(a, expected)
 
 
-class RunStageFromCmdLineTest(SmvBaseTest):
-    stageName = 'runstage.stage1'
-
+class RunStageFromCmdLineTest(RunCmtLineBaseTest):
     @classmethod
-    def smvAppInitArgs(cls):
-        return ['--smv-props', 'smv.stages=runstage.stage1',
-                '-s', cls.stageName]
+    def whatToRun(cls):
+        return ['-s', "runstage.stage1"]
 
     def test_can_run_stage_from_cmdline(self):
         self.smvApp.j_smvApp.run()
-        a = self.df(self.stageName + ".modules.A")
+        a = self.df("runstage.stage1.modules.A")
         self.should_be_same(a, self.createDF("k:String;v:Integer", "a,;b,2"))
-        b = self.df(self.stageName + ".modules.B")
+        b = self.df("runstage.stage1.modules.B")
         self.should_be_same(b, self.createDF("k:String;v:Integer", "c,3;d,4"))
+
+class RunNotExistModuleTest(RunCmtLineBaseTest):
+    @classmethod
+    def whatToRun(cls):
+        return ['-m', 'tooth-fary']
+
+    def test_should_report_non_existing_module(self):
+        with self.assertRaisesRegexp(Py4JJavaError, "Cannot find module"):
+            self.smvApp.run()
+
+class RunModuleAmbiguousTest(RunCmtLineBaseTest):
+    @classmethod
+    def whatToRun(cls):
+        return ['-m', 'A']
+    
+    def test_should_report_ambiguous_modules(self):
+        with self.assertRaisesRegexp(Py4JJavaError, r"Module name \[A\] is not specific enough"):
+            self.smvApp.run()
+
+
+class SmvAppPurgeTest(SmvBaseTest):
+    @classmethod
+    def smvAppInitArgs(cls):
+        return ['--smv-props', 'smv.stages=runstage.stage1',
+            '-m', 'modules.A',
+            '--purge-old-output']
+
+    def tmpOutputDir(self):
+        return self.tmpDataDir() + "/output"
+
+    def createTempOutputFile(self, baseName, fileContents = "xxx"):
+        """create a temp file in the input data dir with the given contents"""
+        import os
+        fullPath = self.tmpOutputDir() + "/" + baseName
+        directory = os.path.dirname(fullPath)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        f = open(fullPath, "w")
+        f.write(fileContents)
+        f.close()
+
+    def test_old_persisted_data_should_be_removed(self):
+        import os
+        # create some dummy old persisted data files
+        self.createTempOutputFile("runstage.stage1.modules.A_111.csv")
+
+        self.smvApp.run()
+        self.assertNotIn(
+            "runstage.stage1.modules.A_111.csv", 
+            os.listdir(self.tmpOutputDir())
+        )
+
