307717a42d8b0703e0c4b58b7f06c9810d645cb3
Bo Zhang <bzhangusc@live.com>
2018-10-11 16:58:34 -0700

    #1390 optimized list_distinct helper

--- src/main/python/smv/utils.py
+++ src/main/python/smv/utils.py
@@ -92,8 +92,10 @@ def list_distinct(l):
     """Return a the distinct version of the input list, perserve order
     """
     uniq_list = []
+    seen = set()
     for x in l:
-        if x not in uniq_list:
+        if x not in seen:
+            seen.add(x)
             uniq_list.append(x)
     return uniq_list
 
