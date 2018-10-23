3af34ad99321d99e090214aa1f2a43c0683d5e3d
Bo Zhang <bzhangusc@live.com>
2018-10-22 20:41:40 -0700

    #1421 added clean up before write

--- src/main/python/smv/smviostrategy.py
+++ src/main/python/smv/smviostrategy.py
@@ -90,6 +90,9 @@ class SmvCsvOnHdfsIoStrategy(SmvIoStrategy):
                 self.smvApp.log.info("Relying on cached result {} for {} found after lock acquired".format(self._csv_path(), self.fqn))
             else:
                 self.smvApp.log.info("No cached result found for {}. Caching result at {}".format(self.fqn, self._csv_path()))
+                # Delete outputs in case data was partially written previously
+                # since `isWritten` test on schema file, this case only happens when schema was written half way
+                self.remove()
                 self.smvApp.j_smvPyClient.persistDF(self._csv_path(), jdf)
         finally:
             slock.unlock()
