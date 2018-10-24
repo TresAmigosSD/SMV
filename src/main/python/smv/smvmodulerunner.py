fb13d799413564eeb0fb194ec848fc71b6d1decd
Bo Zhang <bzhangusc@live.com>
2018-10-24 13:52:54 -0700

    #1421 added comments and pointer to design doc in the runner code

--- src/main/python/smv/smvmodulerunner.py
+++ src/main/python/smv/smvmodulerunner.py
@@ -28,6 +28,9 @@ class SmvModuleRunner(object):
     def run(self):
         # a set of modules which need to run post_action, keep tracking 
         # to make sure post_action run one and only one time for each TX
+        # the set will be updated by _create_df, _create_meta and _force_post
+        # and eventually be emptied out
+        # See docs/dev/SmvGenericModule/SmvModuleRunner.md for details
         mods_to_run_post_action = set(self.visitor.queue)
 
         # a map from urn to already run DF, since the `run` interface of 
@@ -47,23 +50,33 @@ class SmvModuleRunner(object):
         return [known.get(m.urn()) for m in self.roots]
 
     def _create_df(self, known, need_post):
+        # run module and create df. when persisting, post_action 
+        # will run on current module and all upstream modules
         def runner(m, state):
             (urn2df, run_set) = state
             m.rdd(urn2df, run_set)
         self.visitor.dfs_visit(runner, (known, need_post))
 
     def _create_meta(self, need_post):
+        # run user meta. when there is actions in user meta creation,
+        # post_action will run on current module and all upstream modules
         def run_meta(m, run_set):
             m.calculate_user_meta(run_set)
         self.visitor.dfs_visit(run_meta, need_post)
 
     def _force_post(self, need_post):
+        # If there are still module left for post_action, force a run here
+        # to run them and all left over on their upstream
         if (len(need_post) > 0):
             self.log.debug("leftover mods need to run post action: {}".format(
                 [m.fqn()  for m in need_post]
             ))
             def force_run(mod, run_set):
                 mod.force_post_action(run_set)
+            # Note: we used bfs_visit here run downstream first
+            # In case of A<-B<-C all need to run, this way will only
+            # need to force action on C, and A and B's post action can 
+            # also be calculated
             self.visitor.bfs_visit(force_run, need_post)
 
     def _persist_meta(self):
