df87cc61508ffde2a8eb893868797c93b67a4caa
laneb <lane.barlow@gmail.com>
2018-11-25 00:11:26 -0800

    set default log handler and format when smv is imported (#1512)

--- src/main/python/smv/__init__.py
+++ src/main/python/smv/__init__.py
@@ -19,9 +19,12 @@ SmvPyModuleLink = SmvModuleLink
 
 import logging
 logger = logging.getLogger(__name__)
-stderr_appender = logging.StreamHandler()
-
-# Default logging setting
+# Default logging settings
 # These may be overridden when SmvApp initializes
+log_formatter = logging.Formatter(
+    fmt='%(asctime)s %(levelname)s %(name)s: %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S')
+stderr_log_appender = logging.StreamHandler()
+stderr_log_appender.setFormatter(log_formatter)
+logger.addHandler(stderr_log_appender)
 logger.setLevel("INFO")
-logger.addHandler(stderr_appender)
