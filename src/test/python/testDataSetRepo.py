abbd2bd9e9038f76400b0d50571c4324eee696e4
Bo Zhang <bzhangusc@live.com>
2018-12-19 09:22:42 -0800

    #1561 made dicover provider find abs

--- src/test/python/testDataSetRepo.py
+++ src/test/python/testDataSetRepo.py
@@ -120,14 +120,14 @@ class DataSetRepoTest(SmvBaseTest):
             # verify provider class names
             all_providers_names = sorted([p.__name__ for p in all_providers.values()])
             self.assertTrue(
-                set(['MyBaseProvider', 'MyConcreteProvider', 'SomeProvider', 'SomeSemi'])
+                set(['MyBaseProvider', 'MyConcreteProvider', 'SomeProvider', 'SomeSemi', 'ModWithProvider'])
                 < set(all_providers_names)
             )
 
             # verify provider type fqns
             all_providers_fqns = sorted(all_providers.keys())
             self.assertTrue(
-                set(['aaa', 'aaa.bbb', 'some', 'somesemi'])
+                set(['aaa', 'aaa.bbb', 'some', 'somesemi', 'modprov'])
                 < set(all_providers_fqns)
             )
 
